1f0dc612fb6f19d3e1818dbf35b56dcdb962efeb
Nico Liao <40799837+nico-liao0918@users.noreply.github.com>
2026-03-05 22:03:36 +0800

    Create global.js

---
+++ global.js
@@ -0,0 +1,33 @@
+// Global.js
+
+// DARK MODE TOGGLE
+const darkToggleBtn = document.getElementById('dark-toggle');
+darkToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+const popup = document.getElementById('popup');
+
+function openPopup() {
+    popup.classList.remove('popup_hidden');
+}
+
+function closePopup() {
+    popup.classList.add('popup_hidden');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(openPopup, 5000);
+});
